ec68373fc897e05562905383a6a840aa63c3874d
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2022-11-12 17:29:49 +0100

    feat: load complex recipient data

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import smtplib
 import ssl
@@ -42,24 +43,62 @@ class Stan:
         Loads sender data from json file and converts it to list of `Sender`s.
         Each record in json file should contain email, smtp_host, smtp_port and password.
         """
+        if not file_path.endswith('.json'):
+            # try to predict if user forgot to use file extension
+            file_path = file_path + '.json'
+
         file = open(file_path)
         data = json.load(file)
         return [self.load_sender_settings(sender) for sender in data]
 
-    def prepare_message_content(self, template_path: str, sender_email: str, recipient_email: str) -> EmailMessage:
-        """Prepare message based on template file."""
+    def load_recipients_from_csv(self, file_path: str) -> List[dict]:
+        """Loading recipients data from csv file."""
+        recipients = []
 
-        html = self.render_jinja_template(template_path, gender="male")
+        if not file_path.endswith('.csv'):
+            # try to predict if user forgot to use file extension
+            file_path = file_path + '.csv'
+
+        with open(file_path) as f:
+            csv_data = csv.reader(f)
+            header = next(csv_data)
+            for row in csv_data:
+                if row:
+                    # csv can contain blank row (`[]`)
+                    recipient = {}
+                    for i, col in enumerate(header):
+                        recipient[col] = row[i]
+                    recipients.append(recipient)
+        return recipients
+
+    def load_recipients_from_json(self, file_path: str) -> List[dict]:
+        """Loading recipients data from json file."""
+
+        if not file_path.endswith('.json'):
+            # try to predict if user forgot to use file extension
+            file_path = file_path + '.json'
+
+        file = open(file_path)
+        data = json.load(file)
+        return data
+
+    def prepare_message_content(self, template_path: str, email_subject: str, sender_email: str, recipient: dict) -> EmailMessage:
+        """
+        Prepare message based on template file.
+        Pass recipient data to jinja renderer.
+        """
+
+        html = self.render_jinja_template(template_path, **recipient)
 
         msg = EmailMessage()
-        msg["Subject"] = "Test email"
+        msg["Subject"] = email_subject
         msg["From"] = sender_email
-        msg["To"] = recipient_email
+        msg["To"] = recipient["email"]
         msg.set_content(html, subtype="html")
         return msg
 
     def send_message(
-        self, sender: Sender, recipients: List[str]
+        self, email_subject: str, sender: Sender, recipients: List[dict]
     ) -> None:
         """Use sender smtp to send a message to list of recipients."""
                 
@@ -70,13 +109,13 @@ class Stan:
             server.login(sender.email, sender.password)
         
             for recipient in recipients:
-                msg = self.prepare_message_content("test_msg.html", sender.email, recipient)
-            server.send_message(msg, sender.email, recipients)
+                msg = self.prepare_message_content("test_msg.html", email_subject, sender.email, recipient)
+                server.send_message(msg, sender.email, recipient["email"])
 
 
 if __name__ == "__main__":
     stan = Stan()
     senders = stan.load_senders_from_json("senders.json")
-    recipients = ["jakubmatyszewski1@gmail.com"]
+    recipients = stan.load_recipients_from_csv("dummy_recipients.csv")
     for sender in senders:
-        stan.send_message(sender, recipients)
+        stan.send_message("This is not a drill", sender, recipients)
